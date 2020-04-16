406fcf5aea022490fa3286bf0a8000bc9d1defda
gerarjon <gerarsuaverdez@gmail.com>
2020-04-15 21:14:45 -0700

    added boilerplate

---
+++ client/build/precache-manifest.d9fd0bac20b3716bf0c24829c9edf9bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "09f41e4428d75c50bde9",
+    "url": "/static/js/main.f2c9e5b0.chunk.js"
+  },
+  {
+    "revision": "f5a0adc8d0bc3fbf7243",
+    "url": "/static/js/2.5145a938.chunk.js"
+  },
+  {
+    "revision": "09f41e4428d75c50bde9",
+    "url": "/static/css/main.bb1f1ce3.chunk.css"
+  },
+  {
+    "revision": "76b233a51dfe3dc3f5fe31127bd03a33",
+    "url": "/index.html"
+  }
+];
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks")
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+
+//Get the default connection
+const db = mongoose.connection;
+
+db.once('open', () => console.log('connected to the database'));
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
